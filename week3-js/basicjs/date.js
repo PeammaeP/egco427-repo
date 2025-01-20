23b2c589c3bdcbba47f34ea38d0fee4425fb3a8a
PeamMiiZ <peemmygg@gmail.com>
2025-01-20 15:13:09 +0700

    add the solution of js exercise

--- week3-js/basicjs/date.js
+++ week3-js/basicjs/date.js
@@ -1,22 +1,33 @@
-// date.html 
-//   Illustrates the use of the Date object by 
+// date.html
+//   Illustrates the use of the Date object by
 //   displaying the parts of a current date and
 //   using two Date objects to time a calculation
-    
+
 // Get the current date
 
-      var today = new Date();
+var today = new Date();
 
 // Fetch the various parts of the date
 
-      var dateString = ;
-      var day = ;
-	  //Write your own code here 
-
+var dateString = today.toLocaleDateString("en-us", { weekday: "long" });
+var dateMonth = today.toLocaleDateString("en-us", { month: "long" });
+var dateYear = today.getFullYear();
+var dateToday = today.getDate();
+var myTime = today.toLocaleTimeString();
+//Write your own code here
 
 // Display the parts
 
-      document.write(
-        "Date: " + dateString + "<br />",
-        "Day: " + day + "<br />");
-	//Write your own code here 
+// document.write("Date: " + dateString + "<br />", "Day: " + day + "<br />");
+document.write(
+  dateString +
+    " " +
+    dateToday +
+    " " +
+    dateMonth +
+    " " +
+    dateYear +
+    " , " +
+    myTime
+);
+//Write your own code here
